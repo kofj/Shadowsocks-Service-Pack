f394d3e1d43b9db044a4c18241f66e55517c9b46
Frank Kung <kfanjian@gmail.com>
2015-01-29 16:01:58 +0800

    Remove not use code.

--- Panel/controllers/home.go
+++ Panel/controllers/home.go
@@ -14,12 +14,3 @@ func (this *HomeController) Get() {
 		this.TplNames = "home.tpl"
 	}
 }
-
-/**
- * Show user the result of send mail
- * @author Frank Kung <kfanjian@gmail.com>
- * @date 2015-01-28 19:49:11
- */
-func SendMail() {
-
-}
